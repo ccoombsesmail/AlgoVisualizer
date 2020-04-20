212aff7319b5dbd39844a6b0524deb33cd5b9f8b
Charles Coombs Esmail <coombs.charles@gmail.com>
2020-04-20 02:11:50 -0700

    make markers smaller

--- src/Components/Util/util.js
+++ src/Components/Util/util.js
@@ -177,8 +177,8 @@ export function renderDynamicGraph() {
         .attr("viewBox", "0 -5 10 10")
         .attr("refX", 15)
         .attr("refY", -1.5)
-        .attr("markerWidth", 17)
-        .attr("markerHeight", 15)
+        .attr("markerWidth", 5)
+        .attr("markerHeight", 5)
         .attr("orient", "auto")
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
